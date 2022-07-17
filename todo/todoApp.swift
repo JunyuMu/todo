e2edc0177187e6a16f443f4795f5a4871686fa39
Junyu <me@junyu.dev>
2022-07-17 19:54:05 +0800

    first commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Junyu on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
